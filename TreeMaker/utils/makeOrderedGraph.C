291a06ea358fe27c2cdef6df3c2cc630cda5c8f5
Robin Aggleton <robin.aggleton@cern.ch>
2017-08-04 11:51:06 +0200

    Allow graph ordering script to work with TGraphErrors as well

--- TreeMaker/utils/makeOrderedGraph.C
+++ TreeMaker/utils/makeOrderedGraph.C
@@ -1,19 +1,36 @@
 {
   /** Little script to correctly order the points on a TGraphAsymmErrors.
    * This adds the new graph to the original ROOT file.
+   * Depending on what sort of object is stored (TGraphAsymmErrors or TGraphErrors)
+   * you'll need to (un)comment various type-bits below.
    **/
 
-  TFile f("HEEP_SF.root", "UPDATE");
-  TGraphAsymmErrors * tge = (TGraphAsymmErrors*) f.Get("SF_Eta");
+  TFile f("egammaEffi.txt_EGM2D.root", "UPDATE");
+  TString graphName = "grSF1D_0";
+  // TGraphAsymmErrors * tge = (TGraphAsymmErrors*) f.Get(graphName);
+  TGraphErrors * tge = (TGraphErrors*) f.Get(graphName);
+
+  if (tge == nullptr) {
+    throw runtime_error("Couldn't get graph from file");
+  }
 
   std::map<float, int> xvals;
   int N = tge->GetN();
   double * grX = tge->GetX();
   double * grY = tge->GetY();
-  double * grEXHigh = tge->GetEXhigh();
-  double * grEXLow = tge->GetEXlow();
-  double * grEYHigh = tge->GetEYhigh();
-  double * grEYLow = tge->GetEYlow();
+
+  // If using TGraphAsymmErrors
+  // double * grEXHigh = tge->GetEXhigh();
+  // double * grEXLow = tge->GetEXlow();
+  // double * grEYHigh = tge->GetEYhigh();
+  // double * grEYLow = tge->GetEYlow();
+
+  // If using TGraphErrors
+  // In TGraphError, ErrorXhigh = ErrorXlow, same for Y.
+  double * grEXHigh = tge->GetEX();
+  double * grEXLow = tge->GetEX();
+  double * grEYHigh = tge->GetEY();
+  double * grEYLow = tge->GetEY();
 
   // std::map auto sorts by key, whoop, no work for me
   for (int i=0; i<tge->GetN(); i++) {
@@ -38,8 +55,9 @@
     i++;
   }
 
+  // Save as TGraphAsymmErrors as more flexible/uniform wiht other TGraph sources
   TGraphAsymmErrors * tgeNew = new TGraphAsymmErrors(N, &newX[0], &newY[0], &newEXLow[0], &newEXHigh[0], &newEYLow[0], &newEYHigh[0]);
-  tgeNew->SetName("SF_Eta_Ordered");
+  tgeNew->SetName(graphName + "_Ordered");
   tgeNew->Draw("ALP");
   tgeNew->Write();
 }
